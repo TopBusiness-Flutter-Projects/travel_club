b90736ebcd14c62f0200a523323eeb9c0ad80470
TopBusiness110 <topbusiness110@gmail.com>
2024-12-11 08:29:47 +0200

    colors

--- lib/core/utils/app_colors.dart
+++ lib/core/utils/app_colors.dart
@@ -3,14 +3,25 @@ import 'package:flutter/material.dart';
 import 'hex_color.dart';
 
 class AppColors {
-  static Color primary = const Color(0xff3646ff);
-  static Color secondPrimary = HexColor('#F4E07D');
-
-  static Color red = HexColor('#FF0000');
+  static Color primary = HexColor('#009FE3');
+  static Color secondPrimary = HexColor('#252C31');
+  static Color red = HexColor('#EF5667');
+  static Color green = HexColor('#14CE7F');
+  static Color lightWhite = HexColor('#F2F5F7');
+  static Color lightBlue1 = HexColor('#E1EDF2');
+  static Color  lightBlue2= HexColor('#D2E1E8');
+  static Color samawy = HexColor('#B1C9D3');
+  static Color grey = HexColor('#7B9AA7');
+  static Color lbny = HexColor('#A1DAF8');
+  static Color yellow = HexColor('#FFCC23');
+  static Color blue = HexColor('#003B64');
+  static Color white = Colors.white;
+  
+  // static Color red = HexColor('#FF0000');
   static Color black = Colors.black;
   static Color blackLite = Colors.black12;
   static Color success = Colors.green;
-  static Color white = Colors.white;
+
   static Color error = Colors.red;
   static Color transparent = Colors.transparent;
 
