c6dbc5a12114fe0128b155b114a027d3fcba2c82
TopBusiness110 <topbusiness110@gmail.com>
2024-12-11 10:48:10 +0200

    edit size

--- lib/core/widgets/custom_button.dart
+++ lib/core/widgets/custom_button.dart
@@ -10,23 +10,24 @@ class CustomButton extends StatelessWidget {
     this.onTap,
     required this.title,
   });
- final  void Function()? onTap;
- final String title;
+  final void Function()? onTap;
+  final String title;
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.symmetric(
-          horizontal: getSize(context) / 12, vertical: getSize(context) / 32),
+          horizontal: getWidthSize(context) / 12,
+          vertical: getWidthSize(context) / 32),
       child: InkWell(
         onTap: onTap,
-        borderRadius: BorderRadius.circular(getSize(context) / 12),
+        borderRadius: BorderRadius.circular(getWidthSize(context) / 12),
         child: Container(
           width: double.infinity,
           alignment: Alignment.center,
-          padding: EdgeInsets.all(getSize(context) / 32),
+          padding: EdgeInsets.all(getWidthSize(context) / 32),
           decoration: BoxDecoration(
             color: AppColors.primary,
-            borderRadius: BorderRadius.circular(getSize(context) / 12),
+            borderRadius: BorderRadius.circular(getWidthSize(context) / 12),
           ),
           child: Text(
             title,
--- lib/features/on_boarding/screen/onboarding1.dart
+++ lib/features/on_boarding/screen/onboarding1.dart
@@ -24,13 +24,13 @@ class OnBoarding1 extends StatelessWidget {
           body: Column(
             children: [
               SizedBox(
-                height: getSize(context) / 22,
+                height: getWidthSize(context) / 22,
               ),
               Flexible(
                 fit: FlexFit.tight,
                 child: Center(
                   child: Padding(
-                    padding: EdgeInsets.all(getSize(context) / 22),
+                    padding: EdgeInsets.all(getWidthSize(context) / 22),
                     child: Image.asset(
                       ImageAssets.introBackgroundImage,
                       // width: getSize(context) / 1.1,
@@ -38,29 +38,29 @@ class OnBoarding1 extends StatelessWidget {
                   ),
                 ),
               ),
-              SizedBox(height: getSize(context) / 12),
+              SizedBox(height: getWidthSize(context) / 12),
 
               // SizedBox(height: getSize(context) / 12),
               Container(
-                padding:
-                    EdgeInsets.symmetric(horizontal: getSize(context) / 44),
+                padding: EdgeInsets.symmetric(
+                    horizontal: getWidthSize(context) / 44),
                 child: Text(
                   'نجاحك في البيع يبدأ هنا',
                   textAlign: TextAlign.center,
                   style: TextStyle(
                       fontFamily: 'Tajawal',
                       fontWeight: FontWeight.bold,
-                      fontSize: getSize(context) / 18),
+                      fontSize: getWidthSize(context) / 18),
                 ),
               ),
               Container(
-                padding: EdgeInsets.all(getSize(context) / 44),
+                padding: EdgeInsets.all(getWidthSize(context) / 44),
                 child: Text(
                   'أطلق إمكانياتك كمندوب مبيعات، وتابع فرصك وصفقاتك بكل سهولة.',
                   style: TextStyle(
                       fontWeight: FontWeight.w600,
                       fontFamily: 'Tajawal',
-                      fontSize: getSize(context) / 22),
+                      fontSize: getWidthSize(context) / 22),
                 ),
               ),
 
