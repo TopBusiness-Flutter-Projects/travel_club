24b4efa096c44502c375bb6f6d584af961a36a56
nehal <nehalelsamoly123@gmail.com>
2024-12-11 12:56:28 +0200

    app icon and new pass

--- lib/core/utils/app_translations.dart
+++ lib/core/utils/app_translations.dart
@@ -2,4 +2,6 @@ import 'package:easy_localization/easy_localization.dart';
 
 class AppTranslations {
   static  String hi = 'hi'.tr();
+  static  String back = 'back'.tr();
+  static  String writeNewPass = 'write_new_pass'.tr();
 }
