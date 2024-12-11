ca64345760c9c16d417aeb6875f76de502e1a058
TopBusiness110 <topbusiness110@gmail.com>
2024-12-11 09:30:52 +0200

    uuu

--- lib/features/main_screen/cubit/cubit.dart
+++ lib/features/main_screen/cubit/cubit.dart
@@ -1,11 +1,8 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:travel_club/core/remote/service.dart';
-
 import '../data/repo/main_repo.dart';
 import 'state.dart';
 
 class MainCubit extends Cubit<MainState> {
   MainCubit(this.api) : super(MainInitial());
-
   MainRepoImpl api;
 }
