52505f2adb2bd32fca27f7add8c3a3c0289e4abb
nehal <nehalelsamoly123@gmail.com>
2024-12-11 09:11:30 +0200

    images and icons added

--- lib/core/utils/assets_manager.dart
+++ lib/core/utils/assets_manager.dart
@@ -5,15 +5,29 @@ const String lottiePath = 'assets/lottie';
 class ImageAssets {
   //images
   static const String appIcon = '$imagePath/app_icon.png';
-  static const String logoImage = '$imagePath/1.png';
+  static const String logoImage = '$imagePath/logo_app.png';
   static const String profileDefault = '$imagePath/1.png';
   static const String topbusinessImage = '$imagePath/topbusiness.png';
   static const String introBackgroundImage = '$imagePath/on1.png';
   static const String introBackgroundImage2 = '$imagePath/on2.png';
+  static const String email = '$imagePath/email.png';
+  static const String mask = '$imagePath/mask_appbars.png';
+  static const String profile = '$imagePath/profile.png';
+  static const String rate = '$imagePath/rate.png';
+
   // icons
   static const String phoneIcon = '$iconsPath/phone.svg';
 
   //Lottie
 
   static const String noLessons = '$lottiePath/no_lessons.json';
+}
+class AppIcons{
+  static const String bed = '$iconsPath/bed.svg';
+  static const String camps = '$iconsPath/camps.svg';
+  static const String chalets = '$iconsPath/chalets.svg';
+  static const String container = '$iconsPath/container.svg';
+  static const String correct_green = '$iconsPath/correct_green.svg';
+  static const String entertainment= '$iconsPath/entertainment.svg';
+
 }
