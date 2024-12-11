bc75ea12f26918917815eb2b63b25575892f62ff
nehal <nehalelsamoly123@gmail.com>
2024-12-11 09:48:46 +0200

    edit structure login_repo_impl.dart

--- lib/features/login/data/login_repo.dart
+++
@@ -1,6 +0,0 @@
-import 'package:travel_club/core/api/dio_consumer.dart';
-
-class LoginRepo {
-  DioConsumer api;
-  LoginRepo(this.api);
-}
--- lib/injector.dart
+++ lib/injector.dart
@@ -1,61 +1,78 @@
 import 'package:dio/dio.dart';
-import 'package:flutter/foundation.dart';
-import 'package:travel_club/features/login/cubit/cubit.dart';
-import 'package:travel_club/features/login/data/login_repo.dart';
-import 'package:travel_club/features/main_screen/cubit/cubit.dart';
-import 'package:travel_club/features/splash/cubit/cubit.dart';
+
 import 'package:get_it/get_it.dart';
-import 'package:pretty_dio_logger/pretty_dio_logger.dart';
-
 import 'package:shared_preferences/shared_preferences.dart';
-
 import 'core/api/app_interceptors.dart';
 import 'core/api/base_api_consumer.dart';
 import 'core/api/dio_consumer.dart';
+import 'features/login/cubit/cubit.dart';
+import 'features/login/data/login_repo_impl.dart';
+import 'features/main_screen/cubit/cubit.dart';
 import 'features/main_screen/data/repo/main_repo.dart';
+import 'features/splash/cubit/cubit.dart';
+
 
 // import 'features/downloads_videos/cubit/downloads_videos_cubit.dart';
 
 final serviceLocator = GetIt.instance;
 
 Future<void> setup() async {
-//!-------------------------Declare Cubit-------------------------
+  //! Features
 
-  serviceLocator.registerFactory(
-    () => SplashCubit(),
-  );
+  ///////////////////////// Blocs ////////////////////////
 
-  serviceLocator.registerFactory(
-    () => LoginCubit(
-      serviceLocator(),
-    ),
-  );
-  serviceLocator.registerFactory(
-    () => MainCubit(
-      serviceLocator(),
-    ),
-  );
-//!----------------------------------------------------------------
-///////////////////////////////////////////////////////////////////
-//!-------------------------Declare Repo---------------------------
-  serviceLocator.registerLazySingleton(() => LoginRepo(serviceLocator()));
-  serviceLocator.registerLazySingleton(() => MainRepoImpl(serviceLocator()));
+  serviceLocator.registerFactory(() => SplashCubit());
 
-//!----------------------------------------------------------------
+  // serviceLocator.registerFactory(
+  //   () => LoginCubit(serviceLocator()),
+  // );
+  serviceLocator.registerFactory(
+        () => MainCubit(serviceLocator()),
+  ); serviceLocator.registerFactory(
+        () => LoginCubit(serviceLocator()),
+  );
+  // serviceLocator.registerFactory(() => OnboardingCubit(serviceLocator()));
+  // serviceLocator.registerFactory(() => HomeCubit(serviceLocator()));
+  // serviceLocator.registerFactory(() => PaymentCubit(serviceLocator()));
+  // serviceLocator.registerFactory(() => DetailsCubit(serviceLocator()));
+  // serviceLocator.registerFactory(() => ChatCubit(serviceLocator()));
+  // serviceLocator.registerFactory(() => EventCubit(serviceLocator()));
+  // serviceLocator.registerFactory(() => ProfileCubit(serviceLocator()));
+  // serviceLocator.registerFactory(() => PlacedetailsCubit(serviceLocator()));
+  // serviceLocator.registerFactory(() => MyTripCubit(serviceLocator()));
+  // serviceLocator.registerFactory(() => ChooseRoomCubit(serviceLocator()));
+  // serviceLocator.registerFactory(() => FavouriteCubit(serviceLocator()));
+  // serviceLocator.registerFactory(() => EditCubit(serviceLocator()));
+  // serviceLocator.registerFactory(() => SearchHomeCubit(serviceLocator()));
+  // serviceLocator.registerFactory(() => NotificationCubit(serviceLocator()));
+  // serviceLocator.registerFactory(() => SettingCubit(serviceLocator()));
+  // serviceLocator.registerFactory(() => VisaCardCubit(serviceLocator()));
+  // serviceLocator.registerFactory(() => SupportCubit(serviceLocator()));
+  // serviceLocator.registerFactory(() => AuthCubit(serviceLocator()));
+
+  ///////////////////////////////////////////////////////////////////////////////
 
   //! External
   // Shared Preferences
   final sharedPreferences = await SharedPreferences.getInstance();
   serviceLocator.registerLazySingleton(() => sharedPreferences);
 
-  ///! (dio)
+  // serviceLocator.registerLazySingleton(() => ServiceApi(serviceLocator()));
+  serviceLocator.registerLazySingleton(() => LoginRepoImpl(serviceLocator()));
+  serviceLocator.registerLazySingleton(() => MainRepoImpl(serviceLocator()));
+  // serviceLocator.registerLazySingleton(() => ServiceApiOnBoarding(serviceLocator()));
+  // serviceLocator.registerLazySingleton(() => RepoImplForAuth(serviceLocator()));
+  // serviceLocator.registerLazySingleton(() => HomeRepoImpl(serviceLocator()));
+  // serviceLocator.registerLazySingleton(() => PlaceDetailRpoImpl(serviceLocator()));
+  // serviceLocator.registerLazySingleton(() => DetailsHotelRepoImpl(serviceLocator()));
+
   serviceLocator.registerLazySingleton<BaseApiConsumer>(
-      () => DioConsumer(client: serviceLocator()));
+          () => DioConsumer(client: serviceLocator()));
   serviceLocator.registerLazySingleton(() => AppInterceptors());
 
   // Dio
   serviceLocator.registerLazySingleton(
-    () => Dio(
+        () => Dio(
       BaseOptions(
         contentType: "application/x-www-form-urlencoded",
         headers: {
@@ -65,26 +82,14 @@ Future<void> setup() async {
       ),
     ),
   );
-  serviceLocator.registerLazySingleton(() => PrettyDioLogger(
-            requestHeader: true,
-            requestBody: true,
-            responseBody: true,
-            responseHeader: false,
-            error: true,
-            compact: true,
-            maxWidth: 90,
-            enabled: kDebugMode,
-            // filter: (options, args){
-
-            //   }
-          )
-      //    LogInterceptor(
-      //     request: true,
-      //     requestBody: true,
-      //     requestHeader: true,
-      //     responseBody: true,
-      //     responseHeader: true,
-      //     error: true,
-      //   ),
-      );
+  serviceLocator.registerLazySingleton(
+        () => LogInterceptor(
+      request: true,
+      requestBody: true,
+      requestHeader: true,
+      responseBody: true,
+      responseHeader: true,
+      error: true,
+    ),
+  );
 }
