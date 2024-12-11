72100605de83685ea1b4bc21684a96cc1a481366
nehal <nehalelsamoly123@gmail.com>
2024-12-11 13:18:00 +0200

    customs

--- lib/features/auth/widgets/custom_forward.dart
+++ lib/features/auth/widgets/custom_forward.dart
@@ -3,12 +3,12 @@ import 'package:flutter_svg/svg.dart';
 import '../../../core/exports.dart';
 
 class CustomForward extends StatelessWidget {
-  const CustomForward({super.key});
-
+  const CustomForward({super.key,this.onTap});
+ final void Function()? onTap;
   @override
   Widget build(BuildContext context) {
     return  GestureDetector(
-      onTap: (){},
+      onTap:onTap,
         child: SvgPicture.asset(AppIcons.forward));
   }
 }
