212acc2798bd4a0a73b175feb121306053fff167
TopBusiness110 <topbusiness110@gmail.com>
2024-12-12 08:46:57 +0200

    edit translate

--- lib/core/utils/app_translations.dart
+++ lib/core/utils/app_translations.dart
@@ -1,49 +1,48 @@
 import 'package:easy_localization/easy_localization.dart';
 
 class AppTranslations {
-  static  String hi = 'hi'.tr();
-  static  String back = 'back'.tr();
-  static  String login = 'login'.tr();
-  static  String skip = 'skip'.tr();
-  static  String onboarding1 = 'onboarding1'.tr();
-  static  String onboarding2 = 'onboarding2'.tr();
-  static  String onboarding3 = 'onboarding3'.tr();
-  static  String writeNewPass = 'write_new_pass'.tr();
-  static  String writePass = 'write_pass'.tr();
-  static  String newPass = 'new_pass'.tr();
-  static  String phone = 'phone'.tr();
-  static  String writePhone = 'write_phone'.tr();
-  static  String pass = 'pass'.tr();
-  static  String confirmNewPass = 'confirm_new_pass'.tr();
-  static  String welcome = 'welcome'.tr();
-  static  String accommodation = 'accommodation'.tr();
-  static  String transportation = 'transportation'.tr();
-  static  String food = 'food'.tr();
-  static  String entertainment = 'entertainment'.tr();
-  static  String otherServices = 'other_services'.tr();
-  static  String search = 'search'.tr();
-  static  String newOffers = 'new_offers'.tr();
-  static  String more = 'more'.tr();
-  static  String bestBags = 'best_bags'.tr();
-  static  String home = 'home'.tr();
-  static  String myBookings = 'my_bookings'.tr();
-  static  String favorites = 'favorites'.tr();
-  static  String myAccount = 'my_account'.tr();
+  static String hi = 'hi'.tr();
+  static String back = 'back'.tr();
+  static String login = 'login'.tr();
+  static String skip = 'skip'.tr();
+  static String onboarding1 = 'onboarding1'.tr();
+  static String onboarding2 = 'onboarding2'.tr();
+  static String onboarding3 = 'onboarding3'.tr();
+  static String writeNewPass = 'write_new_pass'.tr();
+  static String writePass = 'write_pass'.tr();
+  static String newPass = 'new_pass'.tr();
+  static String phone = 'phone'.tr();
+  static String writePhone = 'write_phone'.tr();
+  static String pass = 'pass'.tr();
+  static String confirmNewPass = 'confirm_new_pass'.tr();
+  static String welcome = 'welcome'.tr();
+  static String accommodation = 'accommodation'.tr();
+  static String transportation = 'transportation'.tr();
+  static String food = 'food'.tr();
+  static String entertainment = 'entertainment'.tr();
+  static String otherServices = 'other_services'.tr();
+  static String search = 'search'.tr();
+  static String newOffers = 'new_offers'.tr();
+  static String more = 'more'.tr();
+  static String bestBags = 'best_bags'.tr();
+  static String home = 'home'.tr();
+  static String myBookings = 'my_bookings'.tr();
+  static String favorites = 'favorites'.tr();
+  static String myAccount = 'my_account'.tr();
 
   static String fullName = 'full_name'.tr();
   static String writeFullName = 'write_full_name'.tr();
   static String signUp = 'sign_up'.tr();
   static String terms = "terms_and_conditions".tr();
   static String termsAndConditions = "by_signing_up_you_agree_to_our".tr();
-  static String you_can_login = "You_can_log_in_with".tr();
-  static String forget_pass = 'forget_pass'.tr();
+  static String youCanLogin = "You_can_log_in_with".tr();
+  static String forgetPass = 'forget_pass'.tr();
 
   static String havenotAccount = 'donot_have_an_account'.tr();
   static String createNewAccount = 'create_an_account_now'.tr();
   static String google = 'google'.tr();
   static String facebook = 'facebook'.tr();
   static String phoneNumberIsRequired = 'phone_number_is_required'.tr();
-  static String pleaseEnterValidPhoneNumber = 'please_enter_a_valid_phone_number'.tr();
-  
+  static String pleaseEnterValidPhoneNumber =
+      'please_enter_a_valid_phone_number'.tr();
 }
- 
