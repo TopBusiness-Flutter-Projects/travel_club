e08a636f10ac11a4575ab443e65d716b2b87c2d7
nehal <nehalelsamoly123@gmail.com>
2024-12-26 14:56:54 +0200

    all done editing

--- lib/features/food/screens/details_of_food/screens/book_table_screen.dart
+++ lib/features/food/screens/details_of_food/screens/book_table_screen.dart
@@ -1,3 +1,4 @@
+import 'package:travel_club/core/widgets/custom_button.dart';
 import 'package:travel_club/core/widgets/custom_text_form_field.dart';
 
 import '../../../../../core/exports.dart';
@@ -14,16 +15,24 @@ class BookTableScreen extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-
                 SizedBox(height:getHeightSize(context) * 0.05),
                 SizedBox(height: 20.h,),
                 CustomContainerFood(isBooking: false,),
                 SizedBox(height: 20.h,),
                 Text(AppTranslations.selectBookingDate,style: getMediumStyle(fontSize: 14.sp),),
-                CustomTextField(),
+                CustomTextField(suffixIcon: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SvgPicture.asset(AppIcons.calendar),
+                ),
+                  hintText: "١٦ يناير ٢٠٢٢",
+                ),
                 Text(AppTranslations.numberOfAttendees,style: getMediumStyle(fontSize: 14.sp),),
-                CustomTextField()
-
+                CustomTextField(hintText: "٤ اشخاص",) ,
+                Text(AppTranslations.nameOwner,style: getMediumStyle(fontSize: 14.sp),),
+                CustomTextField() ,
+                Text(AppTranslations.numberOfPhoneContact,style: getMediumStyle(fontSize: 14.sp),),
+                CustomTextField(),
+                CustomButton(title: AppTranslations.bookTable, onTap: (){}),
               ],
             ),
         ));
