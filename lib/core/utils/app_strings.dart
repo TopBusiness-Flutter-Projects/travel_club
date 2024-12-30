f008ca4d4d21d6e4261ca5ae4ebf4a154e77a21a
TopBusiness110 <topbusiness110@gmail.com>
2024-12-30 15:00:07 +0200

    y

--- lib/core/utils/app_strings.dart
+++ lib/core/utils/app_strings.dart
@@ -2,13 +2,13 @@
 export 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class AppStrings {
+  
   static const String appName = 'Traverl Club';
   static const String fontFamily = 'NotoSansArabic';
   static const String noRouteFound = 'No Route Found';
   static const String contentType = 'Content-Type';
   static const String applicationJson = 'application/json';
   static const String testImageUrl = 'https://www.infogrepper.com/wp-content/uploads/2022/10/image-url-for-testing.png';
- 
  static const String locale = 'locale';
    static const String englishCode = 'en';
   static const String arabicCode = 'ar';
