eaa1258430cd72014aba6ffb203186cccaddf99d
nehal <nehalelsamoly123@gmail.com>
2025-02-06 16:03:03 +0200

    transportation_body_booking.dart

--- lib/features/favourites/cubit/favourites_state.dart
+++ lib/features/favourites/cubit/favourites_state.dart
@@ -2,8 +2,13 @@ abstract class FavouritesState {}
 
 class FavouritesInitial extends FavouritesState {}
 class LoadingReservationFavourite extends FavouritesState {}
-class ErrorReservationFavourite extends FavouritesState {}
+class ErrorReservationFavourite extends FavouritesState {
+ final bool oldFav;
+
+ ErrorReservationFavourite({required this.oldFav});
+}
 class LoadedReservationFavourite extends FavouritesState {}
+class ErrorGetReservationFavourite extends FavouritesState {}
 class LoadedFavourite extends FavouritesState {}
 class IndexChanged extends FavouritesState {}
 class ChangeFavourite extends FavouritesState {}
