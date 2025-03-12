e8c373febf3ef1d855dfb61e26047141caa2cd7c
TopBusiness110 <topbusiness110@gmail.com>
2025-03-12 14:16:49 +0200

    Add total price, VAT, and rule fields to FoodReservationDetails model and update SecondBookTableScreen to utilize these fields

--- lib/features/food/screens/details_of_food/screens/second_book_table_screen.dart
+++ lib/features/food/screens/details_of_food/screens/second_book_table_screen.dart
@@ -8,58 +8,67 @@ class SecondBookTableScreen extends StatelessWidget {
   const SecondBookTableScreen({super.key});
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<FoodCubit, FoodState>(
-        builder: (BuildContext context, state) {
-      var cubit = context.read<FoodCubit>();
-      return CustomScreen(
-          appbarTitle: AppTranslations.bookTable,
-          body: Padding(
-            padding: const EdgeInsets.all(12.0),
-            child: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    cubit.getRestaurantDetailsModel?.data?.name ?? "",
-                    style: getSemiBoldStyle(fontSize: 14.sp),
-                  ),
-                  SizedBox(
-                    height: 20.h,
-                  ),
-                  StaggeredGrid.count(
-                    crossAxisCount: 2,
-                    children: List.generate(
-                      cubit.cartItems.length,
-                      (index) => CustomMealContainer(
-                        mealModel: cubit.cartItems[index],
-                        isSecondBooking: true,
+    return WillPopScope(
+        onWillPop: () async {
+          checkGoToHome(context);
+          return false;
+        },
+      child: BlocBuilder<FoodCubit, FoodState>(
+          builder: (BuildContext context, state) {
+        var cubit = context.read<FoodCubit>();
+        return CustomScreen(
+            appbarTitle: AppTranslations.bookTable,
+              appBarOnPresses: () {
+                checkGoToHome(context);
+              },
+            body: Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      cubit.getRestaurantDetailsModel?.data?.name ?? "",
+                      style: getSemiBoldStyle(fontSize: 14.sp),
+                    ),
+                    SizedBox(
+                      height: 20.h,
+                    ),
+                    StaggeredGrid.count(
+                      crossAxisCount: 2,
+                      children: List.generate(
+                        cubit.cartItems.length,
+                        (index) => CustomMealContainer(
+                          mealModel: cubit.cartItems[index],
+                          isSecondBooking: true,
+                        ),
                       ),
                     ),
-                  ),
-                  SizedBox(
-                    height: 20.h,
-                  ),
-                  CustomPricesWidget(
-                    totalPrice: cubit.addFoodReservationModel.data?.totalPrice
-                            .toString() ??
-                        "0",
-                    totalPriceAfterVat: cubit
-                            .addFoodReservationModel.data?.totalPriceAfterVat
-                            .toString() ??
-                        "0",
-                    vat: cubit.addFoodReservationModel.data?.vat.toString() ??
-                        "0",
-                    terms: cubit.getRestaurantDetailsModel?.data?.rule ?? "",
-                    reservationId:
-                        cubit.addFoodReservationModel.data?.reservationId ?? 0,
-                  ),
-                  SizedBox(
-                    height: 5.h,
-                  )
-                ],
+                    SizedBox(
+                      height: 20.h,
+                    ),
+                    CustomPricesWidget(
+                      totalPrice: cubit.addFoodReservationModel.data?.totalPrice
+                              .toString() ??
+                          "0",
+                      totalPriceAfterVat: cubit
+                              .addFoodReservationModel.data?.totalPriceAfterVat
+                              .toString() ??
+                          "0",
+                      vat: cubit.addFoodReservationModel.data?.vat.toString() ??
+                          "0",
+                      terms: cubit.getRestaurantDetailsModel?.data?.rule ?? "",
+                      reservationId:
+                          cubit.addFoodReservationModel.data?.reservationId ?? 0,
+                    ),
+                    SizedBox(
+                      height: 5.h,
+                    )
+                  ],
+                ),
               ),
-            ),
-          ));
-    });
+            ));
+      }),
+    );
   }
 }
